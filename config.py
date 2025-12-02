2d04c3809073ff576a7e58597da7f8f46f29ed58
Leexm-dev <xmlee-wp22@student.tarc.edu.my>
2025-12-02 11:47:25 +0800

    Change database password in config.py
    Updated the database password for security reasons.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "employee.cdjrxelsyeef.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
-custompass = "Bait3273"
+custompass = "bmit273"
 customdb = "employee"
 custombucket = "xm-s3-bucket"
 customregion = "us-east-1"
